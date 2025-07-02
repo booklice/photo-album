b73becf7b7ca7661ff2fba40615344d03d1051a5
booklice <58538763+booklice@users.noreply.github.com>
2025-07-02 21:13:59 +0900

    Update fetch-images.js

--- scripts/fetch-images.js
+++ scripts/fetch-images.js
@@ -3,9 +3,9 @@ const cloudinary = require("cloudinary").v2;
 const fs = require("fs");
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDNAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
 });
 
 async function fetchAllImages() {
@@ -15,7 +15,7 @@ async function fetchAllImages() {
   do {
     const result = await cloudinary.search
       .expression("folder:coffee")
-      .sort_by("created_at", "desc") // ✅ 수정된 부분
+      .sort_by("created_at", "desc")
       .max_results(500)
       .next_cursor(nextCursor)
       .execute();
